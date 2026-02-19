a2f5a710606230c77d920df608457d03f62e48b1
silambu <silambusiran1990@gmail.com>
2026-02-19 15:36:30 +0530

    deploy website

---
+++ chunk-3YBOU45M.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-NIHQCDAK.js";import"./chunk-JWDWOGQ3.js";import"./chunk-HF4NSUKP.js";import"./chunk-SQMKIBSS.js";export{a as Navbar};
---
+++ chunk-JP7V64AR.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-YXFKXVWT.js";import"./chunk-HF4NSUKP.js";import"./chunk-SQMKIBSS.js";export{a as Footer};
